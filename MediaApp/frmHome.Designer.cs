32051e0c18d336269f36f5600a28128d73022d94
jmnelson12 <jacob_m_nelson@live.com>
2018-04-10 11:21:41 -0500

    Design Updates

---
+++ MediaApp/frmHome.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MediaApp
+{
+    partial class MovieApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MovieApp));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnFavorites = new System.Windows.Forms.Button();
+            this.btnWatchLater = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.NavPanel = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel3.SuspendLayout();
+            this.NavPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel3.Controls.Add(this.lblHome);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(167, 65);
+            this.panel3.TabIndex = 0;
+            // 
+            // lblHome
+            // 
+            this.lblHome.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblHome.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.ForeColor = System.Drawing.Color.White;
+            this.lblHome.Location = new System.Drawing.Point(0, 0);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(184, 65);
+            this.lblHome.TabIndex = 3;
+            this.lblHome.Text = "Movie App";
+            this.lblHome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnHome
+            // 
+            this.btnHome.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.Location = new System.Drawing.Point(0, 119);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(165, 79);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnFavorites
+            // 
+            this.btnFavorites.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnFavorites.FlatAppearance.BorderSize = 0;
+            this.btnFavorites.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFavorites.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFavorites.ForeColor = System.Drawing.Color.White;
+            this.btnFavorites.Image = ((System.Drawing.Image)(resources.GetObject("btnFavorites.Image")));
+            this.btnFavorites.Location = new System.Drawing.Point(2, 200);
+            this.btnFavorites.Name = "btnFavorites";
+            this.btnFavorites.Size = new System.Drawing.Size(163, 79);
+            this.btnFavorites.TabIndex = 1;
+            this.btnFavorites.Text = "Favorite Movies";
+            this.btnFavorites.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnFavorites.UseVisualStyleBackColor = true;
+            // 
+            // btnWatchLater
+            // 
+            this.btnWatchLater.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnWatchLater.FlatAppearance.BorderSize = 0;
+            this.btnWatchLater.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWatchLater.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWatchLater.ForeColor = System.Drawing.Color.White;
+            this.btnWatchLater.Image = ((System.Drawing.Image)(resources.GetObject("btnWatchLater.Image")));
+            this.btnWatchLater.Location = new System.Drawing.Point(2, 281);
+            this.btnWatchLater.Name = "btnWatchLater";
+            this.btnWatchLater.Size = new System.Drawing.Size(163, 79);
+            this.btnWatchLater.TabIndex = 1;
+            this.btnWatchLater.Text = "Watch Later";
+            this.btnWatchLater.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnWatchLater.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Image = ((System.Drawing.Image)(resources.GetObject("btnSearch.Image")));
+            this.btnSearch.Location = new System.Drawing.Point(0, 361);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(165, 79);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // NavPanel
+            // 
+            this.NavPanel.Controls.Add(this.btnSearch);
+            this.NavPanel.Controls.Add(this.btnWatchLater);
+            this.NavPanel.Controls.Add(this.btnFavorites);
+            this.NavPanel.Controls.Add(this.btnHome);
+            this.NavPanel.Controls.Add(this.panel3);
+            this.NavPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.NavPanel.Location = new System.Drawing.Point(0, 0);
+            this.NavPanel.Name = "NavPanel";
+            this.NavPanel.Size = new System.Drawing.Size(167, 522);
+            this.NavPanel.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(171, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(635, 522);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // MovieApp
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(808, 522);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.NavPanel);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MovieApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Movie App";
+            this.panel3.ResumeLayout(false);
+            this.NavPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnFavorites;
+        private System.Windows.Forms.Button btnWatchLater;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Panel NavPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
--- MediaApp/frmHome.cs
+++ MediaApp/frmHome.cs
@@ -16,5 +16,6 @@ namespace MediaApp
         {
             InitializeComponent();
         }
+
     }
 }
